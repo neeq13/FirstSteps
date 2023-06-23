49327828a8880db25dc2afc46ca80a9a24b2f99c
Беллигор <neeq13@yandex.ru>
2023-06-23 12:02:29 +0300

    Initial commit

---
+++ desktop/src/ru/berg/game/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package ru.berg.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ru.berg.game.Game;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("First Steps");
+		config.setWindowedMode(1280, 768);
+		new Lwjgl3Application(new Game(), config);
+	}
+}
